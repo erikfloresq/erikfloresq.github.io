8eca4715d09e069827513779b515c0c2a2555d47
Erik Flores <erikfloresq@icloud.com>
2022-10-02 19:03:56 -0500

    Add exportPathMap

--- next.config.js
+++ next.config.js
@@ -4,3 +4,17 @@ const nextConfig = {
   reactStrictMode: true,
 }
 module.exports = withContentlayer(nextConfig)
+
+module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/a/mi-primer-wwdc': { page: '/article', query: { title: 'mi-primer-wwdc' } },
+      '/a/ui-search-viewcontroller': { page: '/article', query: { title: 'ui-search-viewcontroller' } },
+      '/a/usar-json-unittest-spm': { page: '/article', query: { title: 'usar-json-unittest-spm' } },
+    }
+  },
+}
