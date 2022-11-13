0869dc7563b04c25e8ef842f261c274093722a76
Erik Flores <erikfloresq@icloud.com>
2022-11-13 17:25:05 -0500

    Fix favicon

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+            <link rel="shortcut icon" href="/favicon.ico" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
             <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet" />
