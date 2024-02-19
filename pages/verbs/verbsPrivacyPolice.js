b98ca145d0f581d444c691aa1e4c4881ab718483
Erik Flores <erikfloresq@icloud.com>
2024-02-18 21:48:33 -0500

    Fix html tag

--- pages/verbs/verbsPrivacyPolice.js
+++ pages/verbs/verbsPrivacyPolice.js
@@ -1,7 +1,6 @@
 export default function VerbsPrivacyPolice() {
     return (
-        <html>
-            <body className="font-roboto flex flex-col justify-center pt-32 pb-40 p-10">
+        <main className="font-roboto flex flex-col justify-center pt-32 pb-40 p-10">
             <h1 className="mb-2 font-bold text-3xl">Verbs</h1>
             <h2 className="font-bold text-2xl mb-6">Privacy Policy</h2>
             <p>Erik Flores built the Verbs app as a free app. This SERVICE is provided by Erik Flores at no cost and is intended
@@ -16,20 +15,20 @@ export default function VerbsPrivacyPolice() {
             <p className="mb-8">The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions,
                 which is accessible at Verbs, unless otherwise defined in this Privacy Policy.</p>
 
-            <p><strong>Information Collection and Use</strong></p>
+            <p>Information Collection and Use</p>
             <p className="mb-8">For a better experience while using our Service, I may require you to provide us with certain
                 personally identifiable information, including but not limited to add whatever else you collect here, e.g. users name. 
                 The information that I request is retained on your device and is not collected by me in any way.</p>
             
 
-            <p><strong>Log Data</strong></p>
+            <p className="font-bold">Log Data</p>
             <p className="mb-8">I want to inform you that whenever you use my Service, in case of an error in the app I collect
                 data and information (through third party products) on your phone called Log Data. This Log Data
                 may include information such as your devices’s Internet Protocol (“IP”) address, device name,
                 operating system version, configuration of the app when utilising my Service, the time and date
                 of your use of the Service, and other statistics.</p>
 
-            <p><strong>Cookies</strong></p>
+            <p className="font-bold">Cookies</p>
             <p>Cookies are files with small amount of data that is commonly used an anonymous unique identifier.
                 These are sent to your browser from the website that you visit and are stored on your devices’s
                 internal memory.</p>
@@ -39,47 +38,45 @@ export default function VerbsPrivacyPolice() {
                 to your device. If you choose to refuse our cookies, you may not be able to use some portions of
                 this Service.</p>
 
-            <p><strong>Service Providers</strong></p>
+            <p className="font-bold">Service Providers</p>
             <p>I may employ third-party companies and individuals due to the following reasons:</p>
             <ul>
-                <li>To facilitate our Service;</li>
-                <li>To provide the Service on our behalf;</li>
-                <li>To perform Service-related services; or</li>
-                <li>To assist us in analyzing how our Service is used.</li>
+                <li>- To facilitate our Service;</li>
+                <li>- To provide the Service on our behalf;</li>
+                <li>- To perform Service-related services; or</li>
+                <li>- To assist us in analyzing how our Service is used.</li>
             </ul>
             <p className="mb-8">I want to inform users of this Service that these third parties have access to your Personal
                 Information. The reason is to perform the tasks assigned to them on our behalf. However, they
                 are obligated not to disclose or use the information for any other purpose.</p>
 
-            <p><strong>Security</strong></p>
+            <p className="font-bold">Security</p>
             <p className="mb-8">I value your trust in providing us your Personal Information, thus we are striving to use
                 commercially acceptable means of protecting it. But remember that no method of transmission over
                 the internet, or method of electronic storage is 100% secure and reliable, and I cannot
                 guarantee its absolute security.</p>
 
-            <p><strong>Links to Other Sites</strong></p>
+            <p className="font-bold">Links to Other Sites</p>
             <p className="mb-8">This Service may contain links to other sites. If you click on a third-party link, you will be
                 directed to that site. Note that these external sites are not operated by me. Therefore, I
                 strongly advise you to review the Privacy Policy of these websites. I have no control over, and
                 assume no responsibility for the content, privacy policies, or practices of any third-party
                 sites or services.</p>
 
-            <p><strong>Children’s Privacy</strong></p>
+            <p className="font-bold">Children’s Privacy</p>
             <p className="mb-8">This Services do not address anyone under the age of 13. I do not knowingly collect personal
                 identifiable information from children under 13. In the case I discover that a child under 13
                 has provided me with personal information, I immediately delete this from our servers. If you
                 are a parent or guardian and you are aware that your child has provided us with personal
                 information, please contact me so that I will be able to do necessary actions.</p>
 
-            <p><strong>Changes to This Privacy Policy</strong></p>
+            <p className="font-bold">Changes to This Privacy Policy</p>
             <p className="mb-8">I may update our Privacy Policy from time to time. Thus, you are advised to review this page
                 periodically for any changes. I will notify you of any changes by posting the new Privacy Policy
                 on this page. These changes are effective immediately, after they are posted on this page.</p>
 
-            <p><strong>Contact Us</strong></p>
-            <p>If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact
-                me.</p>
-            </body>
-            </html>
+            <p className="font-bold">Contact Us</p>
+            <p>If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me.</p>
+        </main>
     );
 }
