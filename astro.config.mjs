a917dfce4ff88b1aacdbc081dc47ed6045202918
Erik Flores <erikfloresq@icloud.com>
2024-08-17 11:16:57 -0300

    Add Verbs privacy police

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://erikfloresq.github.io',
-	integrations: [mdx(), sitemap()],
+  site: 'https://erikfloresq.github.io',
+  integrations: [mdx(), sitemap()]
 });
