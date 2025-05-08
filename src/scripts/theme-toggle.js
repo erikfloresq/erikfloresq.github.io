d0c3b0bb9263228a2a344f8a3d5ed459e98bb98e
Erik Flores <erikfloresq@icloud.com>
2025-05-07 23:46:17 -0300

    Set icons for change themes

--- src/scripts/theme-toggle.js
+++ src/scripts/theme-toggle.js
@@ -1,12 +1,21 @@
 const toggleTheme = () => {
     const currentTheme = document.documentElement.getAttribute('data-theme');
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    updateButtonText(newTheme);
     document.documentElement.setAttribute('data-theme', newTheme);
     localStorage.setItem('theme', newTheme);
+};
+
+const updateButtonText = (theme) => {
+    const button = document.querySelector('.theme-toggle-button'); // Ajusta el selector si es necesario
+    if (button) {
+        button.textContent = theme === 'dark' ? '☀️' : '🌙';
+    }
 };
 
 // Aplica el tema guardado en localStorage al cargar la página
 document.addEventListener('DOMContentLoaded', () => {
     const savedTheme = localStorage.getItem('theme') || 'light';
     document.documentElement.setAttribute('data-theme', savedTheme);
+    updateButtonText(savedTheme);
 });
