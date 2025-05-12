a3f89c44d98944a4b5903c24aa288431e4af67f3
Erik Flores <erikfloresq@icloud.com>
2025-05-11 21:20:58 -0300

    Add search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://erikfloresq.github.io',
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap(), pagefind()]
 });
